51ac642b6b90ef349d79a455d42dddbb59d33ca0
faridaarifin91-sudo <faridaarifin91@gmail.com>
2025-11-02 19:26:30 +0700

    Add files via upload

--- prakalpro_2025_D_2511531020/src/pekan5/PerulanganFor4.java
+++ prakalpro_2025_D_2511531020/src/pekan5/PerulanganFor4.java
@@ -1,6 +1,7 @@
 package pekan5;
 
 import java.util.Scanner;
+
 public class PerulanganFor4 {
     public static void main(String[] args) {
         int jumlah = 0;
@@ -9,12 +10,11 @@ public class PerulanganFor4 {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan nilai batas = ");
         batas = input.nextInt();
-
         input.close();
-        
+
         for (int i = 1; i <= batas; i++) {
             System.out.print(i);
-            jumlah += i;
+            jumlah = jumlah + i;
 
             if (i < batas) {
                 System.out.print(" + ");
